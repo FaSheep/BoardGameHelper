1f514f6d61058a37d285d11f62fac6a4d4282541
FaSheep <fasheep@outlook.com>
2023-11-30 17:32:55 +0800

    test

--- app/src/main/java/com/fasheep/boardgamehelper/MainActivity.java
+++ app/src/main/java/com/fasheep/boardgamehelper/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//
         Locale locale = getResources().getConfiguration().locale;
         String language = locale.getLanguage();
 
